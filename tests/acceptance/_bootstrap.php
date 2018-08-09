be755e62f554e0886374bb278dd74d314e3ce377
Unknown <dia151378@uvg.edu.gt>
2018-08-09 14:24:36 -0300

    implementando conexion con base de datos

--- tests/acceptance/_bootstrap.php
+++ tests/acceptance/_bootstrap.php
@@ -1,2 +1,11 @@
 <?php
-// This is global bootstrap for autoloading
+$I = new FunctionalTester($scenario);
+$I->amOnPage('/');
+$I->see('Hello');
+$I->seeInDatabase('users', array('id' => 1));
+$I->seeFileFound('running.lock');
+function reconnectToDatabase() {
+    $dbh = $this->getModule('Db')->dbh;
+    $dbh->close();
+    $dbh->open();
+}
